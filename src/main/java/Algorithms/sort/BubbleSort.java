78a9b09aab012c6efcd32da3e92bb399ea0f061c
romankhiropulos <romankhiropulos@gmail.com>
2022-06-10 16:57:11 +0300

    BubbleSort

---
+++ src/main/java/Algorithms/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package Algorithms.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void sort(int[] array) {
+        for (int i = array.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int dummy = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = dummy;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 5, 3, 2};
+        BubbleSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
